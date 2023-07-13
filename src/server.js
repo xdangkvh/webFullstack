9884ab500fd576ced893400638b77740ee9aadb0
ADMIN <dangkvh@gmail.com>
2023-07-13 23:25:02 +0700

    connect to mysql use sequelize

--- src/server.js
+++ src/server.js
@@ -2,17 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 
 let app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
 let port = process.env.PORT || 6969;
-app.listen(port, () =>{
-    console.log("backend nodejs is running on the port :" +port)
+app.listen(port, () => {
+    console.log("backend nodejs is running on the port :" + port)
 })
