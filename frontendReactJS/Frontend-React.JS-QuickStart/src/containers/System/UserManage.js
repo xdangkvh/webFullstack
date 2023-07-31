60c791a1a8d26454c13de76a0f282db9ad3edf81
ADMIN <dangkvh@gmail.com>
2023-07-31 22:22:34 +0700

    CRUD with reactjs and nodejs video #38

--- frontendReactJS/Frontend-React.JS-QuickStart/src/containers/System/UserManage.js
+++ frontendReactJS/Frontend-React.JS-QuickStart/src/containers/System/UserManage.js
@@ -9,7 +9,7 @@ class UserManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            arrUsers: []
         }
     }
     async componentDidMount() {
