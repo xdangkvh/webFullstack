98b8bdbd9033071579d2382aa1fa39044496b083
xuan dang <dangkvh@gmail.com>
2023-07-31 16:42:28 +0700

    CRUD with ReactJS video #37

--- webFullstack/src/route/web.js
+++ webFullstack/src/route/web.js
@@ -11,7 +11,8 @@ let initWebRoutes = (app) => {
     router.get("/edit-crud", homeController.getEditCRUD);
     router.post("/put-crud", homeController.putCRUD);
     router.get("/delete-crud", homeController.deleteCRUD);
-    router.post("/api/login", userController.handleLogin)
+    router.post("/api/login", userController.handleLogin);
+    router.get("/api/get-all-users", userController.handleGetAllUsers);
     return app.use("/", router);
 }
 
